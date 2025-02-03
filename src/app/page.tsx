c5245f18b240e232cfd1378cf683604f8203035d
Saba Muhammad Riaz <sabaranicool94@gmail.com>
2025-02-03 13:51:03 -0800

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Hero from "../components/hero2"
+import NewArrwa from "../components/NewArrwa"
+import Hero2 from "./Hero2/page"
+import Toppicks from "../components/TopPicks"
+import Whatshopping from "../components/Ourblog"
+export default function Home() {
+  return (
+    <div >
+      <Hero/>
+      <Hero2/>
+      <NewArrwa/>
+      <Toppicks/>
+      <Whatshopping/>
+    </div>
+  )}
